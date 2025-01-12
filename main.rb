4b13adad234400e07d48292d5af6a3fff8f1a729
Samuel Barrientos <stbarriento@gmail.com>
2025-01-12 17:36:11 -0500

    typo cleanup

--- main.rb
+++ main.rb
@@ -3,7 +3,6 @@ require_relative './src/project_set'
 
 json = JSON.parse(File.read('input.json'))
 json['sets'].each do |json_set|
-
   project_set = ProjectSet.from_hash(json_set)
   total_reimbursement = project_set.calculate_total_reimbursement
   puts "#{project_set.name} reimbursement: $#{total_reimbursement}.00"
